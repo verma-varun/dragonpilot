8b7443746b64c16bc9418c926e3b9162a7677f04
Varun Verma <varunverma@Varuns-MacBook-Pro.local>
2025-07-06 18:52:09 -0400

    add fingerprint

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -76,6 +76,7 @@ class CAR(StrEnum):
   SILVERADO = "CHEVROLET SILVERADO 1500 2020"
   EQUINOX = "CHEVROLET EQUINOX 2019"
   TRAILBLAZER = "CHEVROLET TRAILBLAZER 2021"
+  GMC_YUKON_2018 = "GMC YUKON 2018"
 
 
 class Footnote(Enum):
@@ -118,6 +119,7 @@ CAR_INFO: Dict[str, Union[GMCarInfo, List[GMCarInfo]]] = {
   ],
   CAR.EQUINOX: GMCarInfo("Chevrolet Equinox 2019-22"),
   CAR.TRAILBLAZER: GMCarInfo("Chevrolet Trailblazer 2021-22"),
+  CAR.GMC_YUKON_2018: GMCarInfo("GMC YUKON 2018"),
 }
 
 
